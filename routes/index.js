5b4ed432648425407781f564406e9db9f799db8c
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-05-30 06:57:37 -0600

    ejemplo

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const signUp = require("./signUp.js")
 const login = require("./login.js")
 const productos = require("./productos.js")
 const clientes = require("./clientes.js")
+const pedidos = require("./pedidos.js")
 
 router.use("/sign-up", signUp)
 router.use("/login", login)
 router.use("/productos", productos)
 router.use("/clientes", clientes)
+router.use("/pedidos", pedidos)
 
 module.exports = router
